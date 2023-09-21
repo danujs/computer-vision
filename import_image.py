2391e3c50c82d1bee863ed2f75f305e1aa8428ee
danujs <86118475+danujs@users.noreply.github.com>
2023-09-21 17:54:15 +0700

    Update import_image.py

--- import_image.py
+++ import_image.py
@@ -11,3 +11,12 @@ cv2.destroyAllWindows()
 
 #Let's take a closer look at how images are stored
 print(input.shape)
+
+"""In addition, shape gives the dimensions of the image array. 
+To find out the height and width, use shape[]"""
+print ('Height of Image:', int(input.shape[0]),'pixels')
+print ('Width of Image:', int(input.shape[1]), 'pixels')
+
+#We can save the image file we edit and rename it
+cv2.imwrite('output.jpg', input)
+cv2.imwrite('output.png', input)
