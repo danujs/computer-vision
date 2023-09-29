b023f6f2b045cbce606a95af8f9ba622b2d45647
danujs <86118475+danujs@users.noreply.github.com>
2023-09-29 23:24:38 +0700

    Update split.py

--- split.py
+++ split.py
@@ -1,5 +1,8 @@
-Let's now explore looking at individual channels in an RGB image
-> OpenCV's split function splits the image into each color index
+#Let's now explore looking at individual channels in an RGB image
+#> OpenCV's split function splits the image into each color index
+
+import cv2
+import numpy as np
 
 image = cv2.imread('onepeacelake.jpeg')
 
@@ -23,3 +26,17 @@ cv2.imshow("Merged with Blue Amplified", merged)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# Let's create a matrix of Zeros
+# with dimensions of the image h x w
+
+
+zeros = np.zeros(image.shape[:2], dtype= "uint8")
+
+cv2.imshow("Red", cv2.merge([zeros, zeros, R])
+cv2.imshow("Green", cv2.merge([zeros, G, zeros]))
+cv2.imshow("Blue", cv2.merge([B, zeros, zeros]))
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+           
